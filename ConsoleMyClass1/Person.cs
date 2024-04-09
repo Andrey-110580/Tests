82a84516e054c7b7002692c077055a6dc976c0e9
Andrey-110580 <ostriy0511@gmail.com>
2024-04-09 22:21:20 +0300

    Create Person.cs

---
+++ ConsoleMyClass1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMyClass1
+{
+    internal class Person
+    {
+
+        public int Id;
+        public string FirstName;
+        public string SecondName;
+        public string LostName;
+        public string Email;
+        public string PhoneNumber;
+        public string Title;
+
+
+    }
+}
