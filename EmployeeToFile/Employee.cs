b01f248d858c94103068b0b2c4d457a78592f386
Andrey-110580 <54639049+Andrey-110580@users.noreply.github.com>
2024-04-26 21:48:28 +0300

    Add FileManager

---
+++ EmployeeToFile/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeToFile
+{
+     public class Employee
+    {
+        private static int _Counter; // static , что бы переменная была доступна всем экземплярам и использовать переменную, без экземпляра
+        public static int Counter
+        { get { return _Counter; } }
+        public Employee()
+        {
+           _Counter ++;  // каждый раз , при создании экземпляра
+        
+        }
+        public int _id;
+        public string _FirstName;
+        public string _SecondName;
+        public string _LastName;
+        public string _Pozition;
+        public string _Country;
+        public string _City;
+        public string _Email;
+        public string _Phone;
+        public string _PostalCode;
+        public DateTime _BirthDate;
+        public bool _IsWork ;
+
+
+
+
+        public string  Serialise()
+        {
+            string _IdStr = _id.ToString();
+           // string _BirthStr = _BirthDate.ToString();
+           // string _IsWorkStr = _IsWork.ToString();
+            string _TextInfo = _IdStr + ";" + _FirstName + ";" + _SecondName + ";" + _LastName + ";" + _Pozition + ";" + _Country + ";" +
+                _City + ";" + _Email + ";" + _Phone + ";" + _PostalCode + ";" + _BirthDate + ";" + _IsWork;
+
+            return _TextInfo;
+
+        }
+
+        public static Employee Deserialise (string str)
+        {
+ 
+            string[] data = str.Split(';');
+            Employee emp = new Employee();
+
+            emp._id = Convert.ToInt32 (data[0]);
+            emp._FirstName = data[1];
+            emp._SecondName = data[2];
+            emp._LastName = data[3];
+            emp._Pozition = data[4];    
+            emp._Country = data[5];
+            emp._City = data[6];
+            emp._Email = data[7];
+            emp._Phone = data[8];
+            emp._PostalCode = data[9];
+            emp._BirthDate = Convert.ToDateTime(data[10]);
+            emp._IsWork = Convert.ToBoolean(data[11]);
+
+
+            return emp;
+        }
+
+
+
+
+    }
+}
---
+++ EmployeeToFile/EmployeeFileManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeToFile
+{
+    public class EmployeeFileManager
+    {
+        const string path = @"G:\app\export.txt";
+        private string DataToText;
+
+        private string[] EmplArr = new string[Employee.Counter];
+
+        // private List<Employee> employees = new List<Employee>();
+
+
+       // static void AddToList (List<Employee> employees )
+      //  {
+      //      employees.Add ( new Employee() );
+      //  }
+
+        public  void Export(Employee empl)
+        {
+            DataToText = empl.Serialise();
+            File.AppendAllText(path, DataToText);
+            Console.WriteLine(  "Файл создан");
+        }
+
+        public static void Import() 
+        {
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                string fileText = File.ReadAllText(path);
+
+                Console.WriteLine( fileText );
+
+                string[] dateFileArr = fileText.Split('\n');
+                foreach (var item in dateFileArr)
+                {
+                    Employee.Deserialise(item);
+                }
+            }
+            
+        }
+
+    }
+}
